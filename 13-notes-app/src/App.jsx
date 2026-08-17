199ce936131e175212655a8150704c9698cfab63
Rishav-009 <rishavsanjorian789@gmail.com>
2026-08-17 17:35:15 +0530

    prepared a app for taking notes using react along with few another features

--- 13-notes-app/src/App.jsx
+++ 13-notes-app/src/App.jsx
@@ -1,24 +1,83 @@
-import React from 'react'
+import {React,useState} from 'react'
 
 const App = () => {
+
+  const [title, settitle] = useState('')
+  const [details, setdetails] = useState('')
+  const [task, settask] = useState([])
+
+  const SubmitHandler = (e)=>{
+    e.preventDefault()
+    console.log(title,details);
+    const copytask=[...task]
+    copytask.push({title,details})
+    settask(copytask)
+    console.log(task)
+    settitle('')
+    setdetails('')
+  }
+  const deleteNode =(idx)=>{
+    const copyTask=[...task]
+    copyTask.splice(idx,1);
+    settask(copyTask)
+  }
   return (
-    <div className='h-screen bg-black  text-white '>
-      <form className='flex justify-between p-10 items-start' >
-          <div className="flex  flex-col w-1/2 gap-5 p-10 ">
-            <input  type='text' 
-            placeholder='Enter task'
-            className='px-5 py-2 w-full font-medium border-2 rounded'
-            />
-            <textarea type='text'
-              placeholder='Enter Details' 
-              className='px-5 py-2 w-full font-medium border-2 rounded h-30'
-            />
-            <button className='cursor-pointer bg-white w-full text-black px-5 py-2 rounded' >
-              Add Notes
-            </button>
-          </div>
-          <img className='h-52 rotate-y-180' src="https://imgs.search.brave.com/yYZbe2je2gGM2utwDlhxniTUrTSF8JaBGSeGMcF6_8s/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvdGh1/bWJuYWlscy8wNDkv/NTc4LzE1NS9zbWFs/bC9hLWJsYWNrLWFu/ZC13aGl0ZS1kcmF3/aW5nLW9mLWEtbWFu/LXdyaXRpbmctcG5n/LnBuZw" alt="" />
+    <div className='h-screen bg-black lg:flex text-white '>
+      <form onSubmit={(e)=>{
+        SubmitHandler(e)
+      }} className='flex lg:w-1/2 flex-col  p-10 items-start gap-5 ' >
+          <input  type='text' 
+          placeholder='Enter task'
+          className='px-5 py-2 w-full font-medium border-2 rounded'
+          value={title}
+          onChange={(e)=>{
+            settitle(e.target.value)
+          }}
+          />
+          <textarea type='text'
+            placeholder='Enter Details' 
+            className='px-5 py-2 w-full font-medium border-2 rounded h-30'
+            value={details}
+            onChange={(e)=>{
+              setdetails(e.target.value)
+            }}
+          />
+          <button className='cursor-pointer bg-white w-full text-black px-5 py-2 rounded' >
+            Add Notes
+          </button>
+          
       </form>
+      <div className=' lg:w-1/2  p-10 lg:border-l-2'>
+        <h1 className='text-xl font-bold'>Your Notes</h1>
+        <div className='flex flex-wrap gap-5 mt-5 h-full overflow-auto scrollbar-none'>
+            {task.map(function(elem, idx) {
+              return (
+                <div key={idx}>
+                    <div className="h-52 w-40 bg-cover rounded-xl bg-white py-8 px-4 bg-[url('https://imgs.search.brave.com/c-bsX0TaSAmWk53Y-j7cUzkeyNefGr0Oi5kqXxHacOg/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly93d3cu/cG5nYWxsLmNvbS93/cC1jb250ZW50L3Vw/bG9hZHMvMTQvTm90/ZWJvb2stUGFwZXIt/Tm8tQmFja2dyb3Vu/ZC10aHVtYi53ZWJw')]">
+                      <h3 className="leading-tight text-black font-bold">
+                        {elem.title}
+                      </h3>
+
+                      <p className="leading-tight text-gray-500 font-medium mt-4">
+                        {elem.details}
+                      </p>
+                    </div>
+
+                    <button
+                      onClick={() => {
+                        deleteNode(idx)
+                      }}
+                      className="w-full cursor-pointer active:scale-95 bg-red-500 py-1 text-xs rounded font-bold text-white"
+                    >
+                      Delete
+                    </button>
+                  </div>
+                )
+              })}
+        </div>
+        
+      </div>
+
     </div>
   )
 }
